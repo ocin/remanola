9926c661a1f3382bb6ea3596916ac5eada723609
Niklas Jakobsson <nico@kamino.home.niconet.se>
2018-06-16 10:48:57 +0200

    Added The Echo and Scream 4

--- Codecs/Lua Codecs/Remanola/Launchpad-Mini.lua
+++ Codecs/Lua Codecs/Remanola/Launchpad-Mini.lua
@@ -2003,6 +2003,77 @@ local item_conf_map = {
 			["Knob V8"]={template="FYellow"},
 		},
 	},
+	["Scream 4 Distortion"]={
+		["Default"]={
+		},
+		["Index"]={
+		},
+		["Main"]={
+			["UDVButton 1-1_2-1"]={template="UDOrange"},
+			["Button 1-2"]={template="BRedOnOff"},
+			["Fader 3"]={template="FOrange"},
+			["UDVButton 1-4_2-4"]={template="UDOrange", inverted=true},
+			["Fader 5"]={template="FOrange"},
+			["Fader 6"]={template="FOrange"},
+			["Fader 7"]={template="FRed"},
+		},
+		["Cut"]={
+			["Button 1-2"]={template="BRedOnOff"},
+			["Knob V4"]={template="FGreen"},
+			["Knob V5"]={template="FGreen"},
+			["Knob V6"]={template="FGreen"},
+		},
+		["Body"]={
+			["Button 1-2"]={template="BRedOnOff"},
+			["Fader 4"]={template="FOrange"},
+			["Fader 5"]={template="FOrange"},
+			["Fader 6"]={template="FOrange"},
+			["UDVButton 1-7_2-7"]={template="UDOrange", inverted=true},
+		},
+	},
+	["The Echo"]={
+		["Default"]={
+		},
+		["Index"]={
+		},
+		["Main"]={
+			["UDVButton 1-1_2-1"]={template="UDOrange"},
+			["UDVButton 1-2_2-2"]={template="UDOrange"},
+			["Button 1-3"]={template="BYellowOnOff"},
+			["Fader 4"]={template="FOrange"},
+			["Fader 6"]={template="FOrange"},
+			["Fader 7"]={template="FOrange"},
+		},
+		["Delay"]={
+			["Fader 2"]={template="FGreen"},
+			["UDVButton 7-2_8-2"]={template="UDGreen"},
+			["Fader 3"]={template="FGreen"},
+			["Button 1-4"]={template="BYellowOnOff"},
+			["Button 4-4"]={template="BYellowOnOff"},
+			["Button 8-5"]={template="BYellowOnOff"},
+			["Knob V6"]={template="FOrange"},
+		},
+		["Feedback"]={
+			["Fader 2"]={template="FOrange"},
+			["Knob V3"]={template="FOrange"},
+			["Button 1-5"]={template="BYellowOnOff"},
+			["Fader 6"]={template="FOrange"},
+			["Fader 7"]={template="FAmber"},
+		},
+		["Color"]={
+			["Fader 2"]={template="FAMber"},
+			["UDVButton 1-3_2-3"]={template="UDAmber"},
+			["Button 1-5"]={template="BYellowOnOff"},
+			["Fader 5"]={template="FGreen"},
+			["Fader 6"]={template="FOrange"},
+		},
+		["Modulation"]={
+			["Knob V2"]={template="FYellow"},
+			["Fader 3"]={template="FOrange"},
+			["Fader 5"]={template="FGreen"},
+			["Fader 6"]={template="FYellow"},
+		},
+	},
 }	
 
 local items = {
