05dc7e346f713a2063a470937853018e7bcf5400
Niklas Jakobsson <nico@kamino.home.niconet.se>
2018-06-16 09:09:39 +0200

    Started adding effects: ECF-42, Audiomatic, Pulveriser

--- Codecs/Lua Codecs/Remanola/Launchpad-Mini.lua
+++ Codecs/Lua Codecs/Remanola/Launchpad-Mini.lua
@@ -1937,6 +1937,72 @@ local item_conf_map = {
 			["Fader 7"]={template="FAmber"},
 		},
 	},
+	["ECF-42 Filter"]={
+		["Default"]={
+		},
+		["Index"]={
+		},
+		["Main"]={
+			["UDVButton 1-1_2-1"]={template="UDOrange"},
+			["UDVButton 1-2_2-2"]={template="UDGreen", inverted=true},
+			["Fader 3"]={template="FGreen"},
+			["Fader 4"]={template="FOrange"},
+			["Fader 5"]={template="FAmber"},
+			["Fader 6"]={template="FYellow"},
+		},
+	},
+	["Audiomatic"]={
+		["Default"]={
+		},
+		["Index"]={
+		},
+		["Main"]={
+			["UDVButton 1-1_2-1"]={template="UDOrange"},
+			["Fader 2"]={template="FRed"},
+			["UDVButton 1-3_2-3"]={template="UDOrange"},
+			["Fader 5"]={template="FOrange"},
+			["Fader 6"]={template="FOrange"},
+			["Fader 7"]={template="FRed"},
+		},
+	},
+	["Pulveriser"]={
+		["Default"]={
+		},
+		["Index"]={
+		},
+		["Main"]={
+			["UDVButton 1-1_2-1"]={template="UDOrange"},
+			["UDVButton 7-1_8-1"]={template="UDYellow"},
+			["Fader 2"]={template="FOrange"},
+			["Fader 3"]={template="FYellow"},
+			["Fader 4"]={template="FOrange"},
+			["Fader 5"]={template="FYellow"},
+			["Fader 6"]={template="FOrange"},
+			["Fader 7"]={template="FRed"},
+		},
+		["Filter"]={
+			["UDVButton 1-2_2-2"]={template="UDGreen"},
+			["Fader 3"]={template="FGreen"},
+			["Fader 4"]={template="FOrange"},
+			["Knob V7"]={template="FYellow"},
+			["Knob V8"]={template="FYellow"},
+		},
+		["Follower"]={
+			["Button 8-1"]={template="BRedOnOff"},
+			["Fader 2"]={template="FOrange"},
+			["Fader 4"]={template="FYellow"},
+			["Fader 5"]={template="FYellow"},
+		},
+		["Tremor"]={
+			["Fader 2"]={template="FGreen"},
+			["UDVButton 7-2_8-2"]={template="UDGreen"},
+			["Button 1-3"]={template="BGreenOnOff"},
+			["UDVButton 1-4_2-4"]={template="UDOrange"},
+			["Button 1-5"]={template="BGreenOnOff"},
+			["Fader 6"]={template="FYellow"},
+			["Knob V8"]={template="FYellow"},
+		},
+	},
 }	
 
 local items = {
