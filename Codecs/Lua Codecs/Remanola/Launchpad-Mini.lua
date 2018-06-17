97838fb7d4b8cb99d1405bbb3a25be7de903dc27
Niklas Jakobsson <nico@kamino.home.niconet.se>
2018-06-17 09:55:02 +0200

    Added Softtube Amp

--- Codecs/Lua Codecs/Remanola/Launchpad-Mini.lua
+++ Codecs/Lua Codecs/Remanola/Launchpad-Mini.lua
@@ -2437,6 +2437,27 @@ local item_conf_map = {
 			["Button 1-5"]={template="BGreenOnOff"},
 		},
 	},
+	["Softtube Amp"]={
+		["Default"]={
+		},
+		["Index"]={
+		},
+		["Main"]={
+			["UDVButton 1-1_2-1"]={template="UDOrange"},
+			["UDVButton 1-2_2-2"]={template="UDOrange"},
+			["UDVButton 3-2_4-2"]={template="UDOrange"},
+			["Button 8-2"]={template="BRedOnOff"},
+			["Fader 3"]={template="FOrange"},
+			["Fader 4"]={template="FRed"},
+			["Fader 7"]={template="FRed"},
+			["Fader 8"]={template="FRed"},
+		},
+		["Eq"]={
+			["Fader 3"]={template="FRed"},
+			["Fader 4"]={template="FRed"},
+			["Fader 5"]={template="FRed"},
+		},
+	},
 }	
 
 local items = {
