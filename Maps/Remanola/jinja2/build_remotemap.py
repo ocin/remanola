624c30766d767865d8beca4a767be16ecf8fcace
Niklas Jakobsson <nico@kashyyk.niconet.se>
2018-06-24 21:07:39 +0200

    Testing rewrite with jinja2 templates to generate remote map

---
+++ Maps/Remanola/jinja2/build_remotemap.py
@@ -0,0 +1,14 @@
+#!/opt/local/bin/python2.7
+
+from jinja2 import Environment, FileSystemLoader
+env = Environment(
+    loader=FileSystemLoader('templates')
+)
+
+remotefilename = 'out/Launchpad-Mini.remotemap'
+
+template = env.get_template('Launchpad-Mini.remotemap.j2')
+
+remotefile = open(remotefilename, 'w')
+
+remotefile.write(template.render(version='1.0.1'))
