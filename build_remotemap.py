c23b2866f452d0aa23016207ed305e5f148dc729
Niklas Jakobsson <nico@mandalore.home.niconet.se>
2020-05-30 19:55:18 +0200

    Rename all .j2 files that has remotemap config to .remotemap

--- build_remotemap.py
+++ build_remotemap.py
@@ -11,7 +11,7 @@ env.keep_trailing_newline = True
 
 remotefilename = '/tmp/remanola-out/Launchpad-Mini-withconfmap.remotemap'
 
-template = env.get_template('remotemap/Launchpad-Mini.remotemap.j2')
+template = env.get_template('remotemap/Launchpad-Mini.remotemap')
 
 remotefile = open(remotefilename, 'w')
 
@@ -19,7 +19,7 @@ remotefile.write(template.render(version='1.0.1'))
 
 remotefilename = '/tmp/remanola-out/Launchpad-Pro-withconfmap.remotemap'
 
-template = env.get_template('remotemap/Launchpad-Pro.remotemap.j2')
+template = env.get_template('remotemap/Launchpad-Pro.remotemap')
 
 remotefile = open(remotefilename, 'w')
 
