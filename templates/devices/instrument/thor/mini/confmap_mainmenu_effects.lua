3c0f8b92f9cf29dc94da2acce90de10d98249976
Niklas Jakobsson <nico@kamino.home.niconet.se>
2020-06-05 21:15:45 +0200

    Migrated Thor to confmap new

--- templates/devices/instrument/thor/mini/confmap_mainmenu_effects.lua
+++
@@ -1,3 +0,0 @@
-                        ["Button 2-8"]={template="BEffectPage"},
-                        ["Button 3-8"]={template="BEffectPage"},
-                        ["Button 4-8"]={template="BEffectPage"},
--- templates/devices/instrument/thor/mini/confmap_mainmenu_filter.lua
+++
@@ -1,3 +0,0 @@
-                        ["Button 2-1"]={template="BFilterPage"},
-                        ["Button 3-1"]={template="BFilterPage"},
-                        ["Button 4-1"]={template="BFilterPage"},
