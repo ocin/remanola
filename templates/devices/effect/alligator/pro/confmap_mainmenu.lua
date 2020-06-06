7791cd603c283e5e54d3e4647fe68068f96500dc
Niklas Jakobsson <nico@kamino.home.niconet.se>
2020-06-06 17:10:26 +0200

    Migrated Alligator to new confmap

--- templates/devices/effect/alligator/pro/confmap_mainmenu.lua
+++
@@ -1,7 +0,0 @@
-                        ["Button B1"]={template="BMainPage"},
-                        ["Button B2"]={template="BAmpPage"},
-                        ["Button B3"]={template="BFilterPage"},
-                        ["Button B4"]={template="BLFOPage"},
-                        ["Button B6"]={template="BEffectPage"},
-                        ["Button B7"]={template="BEffectPage"},
-                        ["Button B8"]={template="BEffectPage"},
