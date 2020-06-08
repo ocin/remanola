2ce322e4e372d94a17a41d14c31f44396a2ea03e
Niklas Jakobsson <nico@mandalore.home.niconet.se>
2020-06-08 21:24:36 +0200

    Migrate scream4 to new confmap

--- templates/devices/effect/scream4distortion/pro/confmap_mainmenu.lua
+++
@@ -1,3 +0,0 @@
-                        ["Button B1"]={template="BMainPage"},
-                        ["Button B2"]={template="BFilterPage"},
-                        ["Button B3"]={template="BEffectPage"},
