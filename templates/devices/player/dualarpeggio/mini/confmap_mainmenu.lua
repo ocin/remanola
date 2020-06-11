f4e647d7d83462380115c0acf1715bb88f848515
Niklas Jakobsson <nico@mandalore.home.niconet.se>
2020-06-11 21:22:11 +0200

    Migrate dualarp to new confmap

--- templates/devices/player/dualarpeggio/mini/confmap_mainmenu.lua
+++
@@ -1,3 +0,0 @@
-                        ["Button 1-1"]={template="BMainPage", helptext="Goto Main"},
-                        ["Button 2-1"]={template="BOscPage", helptext="Goto Arp 1"},
-                        ["Button 3-1"]={template="BOscPage", helptext="Goto Arp 2"},
