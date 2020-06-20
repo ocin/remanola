19c0ad8d914ff8c0be0c9eb5945ff5fcc89edf62
Niklas Jakobsson <nico@mandalore.home.niconet.se>
2020-06-20 17:23:29 +0200

    Fix hooks for handle_changed_pagename

--- templates/lua/include/handle_changed.lua
+++ templates/lua/include/handle_changed.lua
@@ -47,44 +47,10 @@ end
 function handle_changed_pagename(citemindex, citemname)
 	if(string.match(citemname, "PageName")) then
 		g_updateall = true
-		if(g_scopetext == "Europa" or g_scopetext == "Grain") then
-			local ceng = tonumber(string.match(get_current_page(), "Eng (%d)"))
-			if(ceng ~= nil) then
-				g_select["EngineSelect"] = ceng-1
-			end
-
-			local clfo = tonumber(string.match(get_current_page(), "LFO (%d)"))
-			if(clfo ~= nil) then
-				g_select["LFOSelect"] = clfo-1
-			end
-
-			cenv = tonumber(string.match(get_current_page(), "Env (%d)"))
-			if(cenv ~= nil) then
-				g_select["EnvSelect"] = cenv-1
-			end
-
-			if(string.match(get_current_page(), "Phaser")) then
-				g_select["EffectSelect"] = 4
-			elseif(string.match(get_current_page(), "Dist")) then
-				g_select["EffectSelect"] = 2
-			elseif(string.match(get_current_page(), "Eq")) then
-				g_select["EffectSelect"] = 6
-			elseif(string.match(get_current_page(), "Delay")) then
-				g_select["EffectSelect"] = 1
-			elseif(string.match(get_current_page(), "Comp")) then
-				g_select["EffectSelect"] = 3
-			elseif(string.match(get_current_page(), "Reverb")) then
-				g_select["EffectSelect"] = 0
-			end
-		elseif(g_scopetext == "RV7000 Advanced Reverb") then
-			if(string.match(get_current_page(), "Reverb")) then
-				g_select["EditSelect"] = 0
-			elseif(string.match(get_current_page(), "Eq")) then
-				g_select["EditSelect"] = 1
-			elseif(string.match(get_current_page(), "Gate")) then
-				g_select["EditSelect"] = 2
-			end
-		end
+		{% import "devicelist.j2" as d %}
+		{% for device in d.devices %}
+		{% include "devices/" + device.type + "/" + device.name + "/hooks/handle_changed_pagename.lua" ignore missing %}
+		{% endfor %}
 	end
 end
 
